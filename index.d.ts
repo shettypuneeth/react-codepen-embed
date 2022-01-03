4e363f53cfae25c2e20588fe47fdeddc663c341c
Casper <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-03 06:42:59 +0100

    feat: add typings (#7)

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+declare module 'react-codepen-embed' {
+  import * as React from 'react';
+
+  export interface Props {
+    /**
+     * The Id of the pen to embed
+     */
+    hash: string;
+
+    /**
+     * CodePen username
+     */
+    user: string;
+
+    /**
+     * Height of the pen
+     * @default 300
+     */
+    height?: number;
+
+    /**
+     * Loader to render while the pen is being embedded or errors out
+     */
+    loader?: React.ReactNode;
+
+    /**
+     * Default tab to display
+     * @default 'css,result'
+     */
+    defaultTab?: string;
+
+    /**
+     * Theme for the pen
+     * @default 'dark'
+     */
+    themeId?: string;
+
+    /**
+     * Make the preview 'Click-to-Load'
+     * @default true
+     */
+    preview?: boolean;
+
+    /**
+     * Title of the pen
+     */
+    title?: string;
+  }
+
+  const ReactCodepen: React.FC<Props>;
+  export default ReactCodepen;
+}
